65866b59abca8c269700b9146405d909e60630ef
ChristianPastenC <christian_pasten@outlook.com>
2022-06-04 13:14:30 -0500

    add mouse move listener normalizated

--- main.js
+++ main.js
@@ -86,3 +86,12 @@ const animate = () => {
 }
 
 animate();
+
+const mouse = {
+  x: undefined,
+  y: undefined,
+}
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+});
