aae32fde490323e25caa03e6c0a6da30dcd04313
ChristianPastenC <christian_pasten@outlook.com>
2022-06-04 12:55:29 -0500

    modify to taste with dat.gui

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 import {
-  Scene, 
-  PerspectiveCamera, 
-  WebGLRenderer, 
-  Mesh, 
-  PlaneGeometry, 
-  DoubleSide, 
-  MeshPhongMaterial, 
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Mesh,
+  PlaneGeometry,
+  DoubleSide,
+  MeshPhongMaterial,
   DirectionalLight,
   FlatShading
 } from 'three';
+import * as dat from 'dat.gui';
+
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 10,
+    heightSegments: 10,
+  }
+}
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane);
+gui.add(world.plane, 'height', 1, 20).onChange(generatePlane);
+gui.add(world.plane, 'widthSegments', 1, 50).onChange(generatePlane);
+gui.add(world.plane, 'heightSegments', 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new PlaneGeometry(
+    world.plane.width, 
+    world.plane.height, 
+    world.plane.widthSegments, 
+    world.plane.heightSegments
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    array[i + 2] += Math.random();
+  }
+}
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -34,8 +63,8 @@ const planeMaterial = new MeshPhongMaterial({
 const planeMesh = new Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
 
-const {array } = planeMesh.geometry.attributes.position;
-for(let i = 0; i < array.length; i+= 3){
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
   array[i + 2] += Math.random();
 }
 
