5574ccba2869608703d6201315dcee6c9bd86827
ChristianPastenC <christian_pasten@outlook.com>
2022-06-04 11:38:40 -0500

    add first mesh

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -14,4 +14,28 @@ console.log(camera);
 console.log(renderer);
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({
+  color: 0x00FF00
+});
+console.log(boxGeometry);
+console.log(material);
+
+const mesh = new Mesh(boxGeometry, material);
+console.log(mesh);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+}
+
+animate();
