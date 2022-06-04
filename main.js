2eef3764d0b8d4b0089fdf0aacada8fba4e160a8
ChristianPastenC <christian_pasten@outlook.com>
2022-06-04 11:59:18 -0500

    add directional light

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, PlaneGeometry, DoubleSide } from 'three';
+import {
+  Scene, 
+  PerspectiveCamera, 
+  WebGLRenderer, 
+  Mesh, 
+  PlaneGeometry, 
+  DoubleSide, 
+  MeshPhongMaterial, 
+  DirectionalLight
+} from 'three';
 
 const scene = new Scene();
 const camera = new PerspectiveCamera(
@@ -15,18 +24,22 @@ document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-const planeGeometry = new PlaneGeometry(5, 5, 10, 10); 
-const planeMaterial = new MeshBasicMaterial({
+const planeGeometry = new PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new MeshPhongMaterial({
   color: 0xFF0000,
   side: DoubleSide,
 });
 const planeMesh = new Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
 
+const light = new DirectionalLight(0xFFFFFF, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
+
 const animate = () => {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  planeMesh.rotation.x += 0.01;
+  // planeMesh.rotation.x += 0.01;
 }
 
 animate();
